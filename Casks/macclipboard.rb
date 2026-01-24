4a844efea7b4d3a5460cac8a0d9e214cb1d5f6ec
Ramazan K <rakodev@gmail.com>
2026-01-24 22:50:11 +0100

    Update macclipboard to v0.0.2

--- Casks/macclipboard.rb
+++ Casks/macclipboard.rb
@@ -1,6 +1,6 @@
 cask "macclipboard" do
-  version "0.0.1"
-  sha256 "4f06931be780786736e3264edbad73ca75443fa2b6913dfa957bca83a2a2f7b2"
+  version "0.0.2"
+  sha256 "af09388c3452e85b9854ffd068f3ee2ac8acc56c5f86a7926cb8c9218b08ebfb"
 
   url "https://github.com/rakodev/mac-clipboard/releases/download/v#{version}/MacClipboard-Installer.dmg"
   name "MacClipboard"
