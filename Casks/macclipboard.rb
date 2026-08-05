88705d82f840deb7ce821ce6ea4f03cb06d7e258
Ramazan K <rakodev@gmail.com>
2026-08-05 17:25:18 +0200

    Update macclipboard to v0.1.14

--- Casks/macclipboard.rb
+++ Casks/macclipboard.rb
@@ -1,6 +1,6 @@
 cask "macclipboard" do
-  version "0.1.13"
-  sha256 "bbc28823fe19d1957d9fac93380162aa8a12a4418e7f70c2c16df0ccffdd1048"
+  version "0.1.14"
+  sha256 "13f6ab342acd2e13ea6fc61b558bfaa63f95b77b3bc4c40a274a01f27f069c9b"
 
   url "https://github.com/rakodev/mac-clipboard/releases/download/v#{version}/MacClipboard-Installer.dmg"
   name "MacClipboard"
